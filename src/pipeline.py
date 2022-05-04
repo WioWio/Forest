fb6ca37e295ab2f3d76ede16d4c3315b84847292
WioWio <violetta.nosenko@gmail.com>
2022-05-04 13:37:37 +0400

    demo train

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,11 +1,15 @@
-from sklearn.linear_model import LogisticRegression
+#from sklearn.linear_model import LogisticRegression
+from sklearn.cluster import KMeans
+from sklearn.linear_model import LogisticRegression 
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
 def create_pipeline(
-    use_scaler:bool
+    use_scaler:bool,logreg_C:float
 )->Pipeline:
     steps = []
     if use_scaler:
         steps.append(('scaler',StandardScaler()))
+    #steps.append(('classifier',KMeans(n_clusters=n_clusters)))
+    steps.append(('classifier',LogisticRegression(C=logreg_C)))
     return Pipeline(steps=steps)
