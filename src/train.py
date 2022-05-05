39de3851edd7d08332b9e4c11ddfe55e6a3b3b53
WioWio <violetta.nosenko@gmail.com>
2022-05-05 13:52:35 +0400

    k-fold added

--- src/train.py
+++ src/train.py
@@ -4,10 +4,29 @@ from joblib import dump
 import click
 #import mlflow
 import mlflow.sklearn
+import numpy as np
 from sklearn.metrics import accuracy_score, log_loss, v_measure_score, mean_squared_error
+from sklearn.model_selection import KFold
 
 from pipeline import create_pipeline_k_means, create_pipeline_log_reg
 from data import get_dataset
+
+def get_metrics(
+    classifier:str, model, X, y, n_splits:int, random_state:int
+    ) -> list:
+    X = X.to_numpy()
+    accuracy, mse, v_score = [],[],[]
+    splits = KFold(n_splits=n_splits, random_state=random_state, shuffle=True)
+    for train_i, test_i in splits.split(X):
+        y_pred = model.fit(X[train_i],y[train_i]).predict(X[test_i])
+        if classifier=="K-Means":
+            y_pred+=1 #because k-means give labels starting from 0, not 1 like in dataset
+        accuracy.append(accuracy_score(y[test_i],y_pred))
+        mse.append(mean_squared_error(y[test_i],y_pred))
+        v_score.append(v_measure_score(y[test_i],y_pred))
+    accuracy, mse, v_score = np.mean(accuracy), np.mean(mse), np.mean(v_score)
+    metrics = [("Accuracy",accuracy),("Mean Squared Error",mse),("V-Score",v_score)]
+    return metrics
 
 
 @click.command()
@@ -29,12 +48,6 @@ from data import get_dataset
     "--random-state",
     default=42,
     type=int,
-    show_default=True,
-)
-@click.option(
-    "--test-split-ratio",
-    default=0.2,
-    type=click.FloatRange(0, 1, min_open=True, max_open=True),
     show_default=True,
 )
 @click.option(
@@ -71,32 +84,21 @@ def train(
     dataset_path: Path,
     save_model_path: Path,
     random_state: int,
-    test_split_ratio: float,
+    #test_split_ratio: float,
     use_scaler: bool,
     max_iter: int,
     logreg_c: float,
     n_clusters: int,
     classifier:str
 ) -> None:
-    features_train, features_val, target_train, target_val = get_dataset(
-        dataset_path,
-        random_state,
-        test_split_ratio,
-    )
+    features, target = get_dataset(dataset_path)
     if classifier=="K-Means":
         pipeline = create_pipeline_k_means(use_scaler,n_clusters)
     else:
         pipeline = create_pipeline_log_reg(use_scaler,logreg_c,max_iter)
-    pipeline.fit(features_train, target_train)
-    target_pred = pipeline.predict(features_val)
-    if classifier=="K-Means":
-        target_pred+=1 #because k-means give labels starting from 0, not 1 like in dataset
-    accuracy = accuracy_score(target_val, target_pred)
-    click.echo(f"Accuracy: {accuracy}.")
-    v_score = v_measure_score(target_val,target_pred)
-    click.echo(f"V-Score: {v_score}.")
-    mse = mean_squared_error(target_val,target_pred)
-    click.echo(f"Mean Squared Error: {mse}.")
+    metrics = get_metrics(classifier, pipeline, features, target, 5, random_state)
+    for metric in metrics:
+        click.echo(f"{metric[0]}: {metric[1]}")
     dump(pipeline, save_model_path)
     click.echo(f"Model is saved to {save_model_path}.")
 
