e526d300c353f9b9267216b5289e78ac92981421
WioWio <violetta.nosenko@gmail.com>
2022-05-11 12:42:01 +0400

    test getting dataset

--- src/forest_ml/data.py
+++ src/forest_ml/data.py
@@ -5,8 +5,12 @@ import click
 import pandas as pd
 
 
-def get_dataset(csv_path: Path) -> Tuple[pd.DataFrame, pd.Series]:
-    dataset = pd.read_csv(csv_path)
+def get_dataset(
+        csv_path: Path = None,
+        dataset: pd.DataFrame = None
+        ) -> Tuple[pd.DataFrame, pd.Series]:
+    if csv_path:
+        dataset = pd.read_csv(csv_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
     features = dataset.drop(["Id", "Cover_Type"], axis=1)
     target = dataset["Cover_Type"]
---
+++ tests/test_data.py
@@ -0,0 +1,19 @@
+import pytest
+from pathlib import Path
+import pandas as pd
+
+from forest_ml.data import get_dataset
+
+@pytest.fixture
+def path() -> Path:
+    return 'tests\datasamples.csv'
+
+
+def test_get_dataset(path: Path) -> None:
+    features, target = get_dataset(path)
+
+    assert features.shape[1] == 54
+    assert type(target) == pd.Series
+    assert features.shape[0] == target.shape[0]
+    assert target.values.all() in range(1,7)
+    assert target.name == 'Cover_Type'
