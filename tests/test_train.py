409896ebf0c4b60a0797621bb4eb2c3648f7b1f5
WioWio <violetta.nosenko@gmail.com>
2022-05-11 17:43:48 +0400

    add isolated filesystem

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,5 +1,9 @@
-from click.testing import CliRunner
+import os
+from pathlib import Path
+import pandas as pd
 import pytest
+
+from click.testing import CliRunner
 
 from forest_ml.train import train
 
@@ -9,25 +13,75 @@ def runner() -> CliRunner:
     return CliRunner()
 
 
-def test_log_reg_c(
-    runner: CliRunner
+@pytest.fixture
+def save_path() -> Path:
+    return 'model.joblib'
+
+
+@pytest.fixture
+def load_path() -> Path:
+    return 'datasamples.csv'
+
+
+@pytest.fixture
+def data() -> pd.DataFrame:
+    return pd.read_csv('tests/datasamples.csv')
+
+def test_logreg_c(
+    runner: CliRunner,
+    data: pd.DataFrame,
+    load_path: Path,
+    save_path: Path
 ) -> None:
     """It fails when log_reg_c is not btw. 0 and 1."""
-    result = runner.invoke(train, ["--logreg-c", 6])
-    assert result.exit_code == 2
-    assert "Invalid value for '--logreg-c'" in result.output
+    with runner.isolated_filesystem():
+        data.to_csv(load_path)
+        result = runner.invoke(train, ['-s', save_path,
+                                       '-d', load_path,
+                                       "--logreg-c", 6])
+        assert result.exit_code == 2
+        assert "Invalid value for '--logreg-c'" in result.output
 
-    result = runner.invoke(train, ["--logreg-c", 0])
-    assert result.exit_code == 0
-    assert "Model is saved" in result.output
+        result = runner.invoke(train, ['-s', save_path,
+                                       '-d', load_path,
+                                       "--logreg-c", 0])
+        assert result.exit_code == 0
+        assert "Model is saved" in result.output
 
 def test_classifier(
-    runner: CliRunner
+    runner: CliRunner,
+    data: pd.DataFrame,
+    load_path: Path,
+    save_path: Path
 ) -> None:
-    result = runner.invoke(train, ["--classifier", 'Forest']) 
-    assert result.exit_code == 2
-    assert "Invalid value for '--classifier'" in result.output
+    with runner.isolated_filesystem(): 
+        data.to_csv(load_path)
+        result = runner.invoke(train, ['-s', save_path,
+                                       '-d', load_path,
+                                       "--classifier", 'Forest']) 
+        assert result.exit_code == 2
+        assert "Invalid value for '--classifier'" in result.output
 
-    result = runner.invoke(train, ["--classifier", 'LogReg'])
-    assert result.exit_code == 0
-    assert "Model is saved" in result.output
+        result = runner.invoke(train, ['-s', save_path,
+                                       '-d', load_path,
+                                       "--classifier", 'LogReg'])
+        assert result.exit_code == 0
+        assert "Model is saved" in result.output
+
+
+
+
+
+def test_create_model(
+        runner: CliRunner,
+        data: pd.DataFrame,
+        load_path: Path,
+        save_path: Path
+        ):
+    with runner.isolated_filesystem():
+        data.to_csv(load_path)
+        assert not os.path.exists(save_path)
+        runner.invoke(train,
+                      ['-s', save_path,
+                       '-d', load_path])
+        assert os.path.exists(save_path)
